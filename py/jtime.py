8535535e4d2400ab85efdc2f76d9b67cfe38e327
Jakob Serlier <37184788+Jakob-98@users.noreply.github.com>
2022-07-05 14:21:01 +0200

    first commit

---
+++ py/jtime.py
@@ -0,0 +1,14 @@
+class Timer:
+   def __init__(self, printupdates = False):
+    self.starttime = time.time()
+    self.deltatime = time.time()
+    self.printupdates = printupdates
+
+   def updatetime(self, updatemsg: str):
+    if not self.printupdates: return
+    prevtime = self.deltatime
+    self.deltatime = time.time()
+    print(updatemsg, '%.2f' % float(1000*(self.deltatime - prevtime)), 'ms')
+   
+   def totalTime(self, updatemsg: str):
+    print(updatemsg, '%.2f' % float(1000*(time.time() - self.starttime)), 'ms')
